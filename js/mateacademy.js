8f6ca9293c587a0e6ed0b6c89a4d7ab5b393083c
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2022-11-04 15:37:40 +0200

    041122

--- js/mateacademy.js
+++ js/mateacademy.js
@@ -1,11 +1,12 @@
-function makeStickers(detailsCount, robotPart) {
-  let value = [];
-  if (detailsCount === 0) {
-    return [];
-}
-    for (let i = 0; i < robotPart.length; i++) {
+// function makeStickers(detailsCount, robotPart) {
+//   let value = [];
+//   if (detailsCount === 0) {
+//     return [];
+// }
+//     for (let i = 0; i < robotPart.length; i++) {
       
-  }
-}
+//   }
+// }
 
-makeStickers(1, robotPart);
+// makeStickers(1, robotPart);
+
