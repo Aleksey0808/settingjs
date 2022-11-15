c0d6ed822f96cf1f4eb22d8fabf23d6cb2764b4e
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2022-11-15 15:41:27 +0200

    151122

--- js/lesson5.js
+++ js/lesson5.js
@@ -10,44 +10,74 @@
 // console.log(car2)
 // console.log(car.isPrototypeOf('brend'));
 
-const Car = function ({brand, modal, price} = {}) {
-  this.brand = brand;
-  this.modal = modal;
-  this.price = price;
+// const Car = function ({brand, modal, price} = {}) {
+//   this.brand = brand;
+//   this.modal = modal;
+//   this.price = price;
 
+// };
+
+// Car.prototype.sayHi = function () {
+//   // console.log('Car.prototype.sayHi => this', this);
+//   // console.log("Hello:")
+// }
+// // console.log(Car.prototype)
+
+// Car.prototype.chengePrice = function (newPrice) {
+//   this.price = newPrice;
+// }
+
+// const myCar = new Car({
+//   brand: 'Audio',
+//   modal: 'q3',
+//   price: 35000,
+// });
+
+// myCar.chengePrice(45000)
+
+// const myCar2 = new Car({
+//   brand: 'Audio',
+//   modal: 'a6',
+//   price: 25000,
+// })
+
+// const myCar3 = new Car({
+//   brand: 'bmw',
+//   modal: 'x6',
+//   price: 50000,
+// })
+
+// // console.log(Car);
+// console.log(myCar);
+// // console.log(myCar2);
+// // console.log(myCar3);
+
+class Storage {
+  constructor(items) {
+    this.items = items;
+  };
+
+  getItems() {
+  return this.items;
 };
 
-Car.prototype.sayHi = function () {
-  // console.log('Car.prototype.sayHi => this', this);
-  // console.log("Hello:")
+addItem(newItem) {
+  return this.items.push(newItem);;
+};
+
+removeItem(itemToRemove) {
+
+  this.items = this.items.filter(item => item !== itemToRemove);
+};
+
+
 }
-// console.log(Car.prototype)
 
-Car.prototype.chengePrice = function (newPrice) {
-  this.price = newPrice;
-}
 
-const myCar = new Car({
-  brand: 'Audio',
-  modal: 'q3',
-  price: 35000,
-});
-
-myCar.chengePrice(45000)
-
-const myCar2 = new Car({
-  brand: 'Audio',
-  modal: 'a6',
-  price: 25000,
-})
-
-const myCar3 = new Car({
-  brand: 'bmw',
-  modal: 'x6',
-  price: 50000,
-})
-
-// console.log(Car);
-console.log(myCar);
-// console.log(myCar2);
-// console.log(myCar3);
+// Change code above this line
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
