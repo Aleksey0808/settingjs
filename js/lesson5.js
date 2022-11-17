4e1e5fdbd899d81002e48848903b01b35c6b37b6
Aleksey <103374981+Aleksey0808@users.noreply.github.com>
2022-11-17 15:52:28 +0200

    171122

--- js/lesson5.js
+++ js/lesson5.js
@@ -255,5 +255,158 @@
 // poly.updatePostCount(4);
 // console.log(poly.getInfo()); // User poly@mail.com is 19 years old and has 21 posts
 
+// Example 2 - Хранилище
+// Напиши класс Storage который создаёт объекты для управления складом товаров. При вызове будет получать
+//  один аргумент - начальный массив товаров, и записывать его в свойство items.
+
+// Добавь методы класса:
+
+// getItems() - возвращает массив товаров.
+// addItem(item) - получает новый товар и добавляет его к текущим.
+// removeItem(item) - получает товар и, если он есть, удаляет его из текущих.
+// const storage = new Storage(['🍎', '🍋', '🍇', '🍑']);
+
+// class Storage {
+//     constructor(items) {
+//         this.items = items;
+
+//     }
+    
+//     getItems() {
+//        return this.items;
+//     };
+
+//     addItem(item) {
+//         return this.items.push(item);
+//     };
+
+//     removeItem(item) {
+//         this.items.includes(item) ? this.items.splice(1, this.items.indexOf(item)) : this.items
+//     };
+
+// }
+
+// const storage = new Storage(['🍎', '🍋', '🍇', '🍑']);
+
+// const items = storage.getItems();
+// console.table(items); // [ '🍎', '🍋', '🍇', '🍑' ]
+
+// storage.addItem('🍌');
+// console.table(storage.items); // [ '🍎', '🍋', '🍇', '🍑', '🍌' ]
+
+// storage.removeItem('🍋');
+// console.table(storage.items); // [ '🍎', '🍇', '🍑', '🍌' ]
+
+// Example 3 - User
+// Напиши класс User который создаёт объект со свойствами login и email. 
+// Объяви приватные свойства #login и #email, доступ к которым сделай через геттер и сеттер login и email.
+
+// class User {
+//     #login;
+//     #email;
+
+//     constructor({login, email} = {}) {
+//         this.#login = login;
+//         this.#email = email;
+//     }
+
+//     get login() {
+//         return this.#login;
+//       }
+    
+//       set login(newLogin) {
+//         this.#login = newLogin;
+//       }
+
+//       get email() {
+//         return this.#email;
+//       }
+    
+//       set email(newEmail) {
+//         this.#email = newEmail;
+//       }
+
+// }
+
+// const mango = new User({
+//   login: 'Mango',
+//   email: 'mango@dog.woof',
+// });
+
+// console.log(mango.login); // Mango
+// mango.login = 'Mangodoge';
+// console.log(mango.login); // Mangodoge
+
+// const poly = new User({
+//   login: 'Poly',
+//   email: 'poly@mail.com',
+// });
+
+// console.log(poly.login); // Poly
+// poly.login = 'Polycutie';
+// console.log(poly.login); // Polycutie
+
+// Example 4 - Заметки
+// Напиши класс Notes который управляет коллекцией заметок в свойстве items. 
+// Заметка это объект со свойствами text и priority. Добавь классу статическое свойство Priority,
+//  в котором будет храниться объект с приоритетами.
+
+// {
+//   LOW: 'low',
+//   NORMAL: 'normal',
+//   HIGH: 'high'
+// }
+// Добавь методы addNote(note), removeNote(text) и updatePriority(text, newPriority).
+
+// class Notes {
+//     static Priority
+//     constructor(items) {
+//         items = items;
+//     }
+// }
+
+// const myNotes = new Notes([]);
+
+// myNotes.addNote({ text: 'Моя первая заметка', priority: Notes.Priority.LOW });
+// console.log(myNotes.items);
+
+// myNotes.addNote({
+//   text: 'Моя вторая заметка',
+//   priority: Notes.Priority.NORMAL,
+// });
+// console.log(myNotes.items);
+
+// myNotes.removeNote('Моя первая заметка');
+// console.log(myNotes.items);
+
+// myNotes.updateNote('Моя вторая заметка', Notes.Priority.HIGH);
+// console.log(myNotes.items);
+
+// Example 5 - Toggle
+// Напишите класс Toggle который принимает объект настроек {isOpen: boolean} 
+// и объявляет одно свойство on - состояние вкл/выкл (true/false). 
+// По умолчанию значение свойства on должно быть false.
+
+class Toggle {
+    constructor({isOpen: boolean} = {}, on = false) {
+        this.on = on;
+    }
+}
+
+const firstToggle = new Toggle({ isOpen: true });
+
+console.group('firstToggle');
+console.log(firstToggle.on);
+firstToggle.toggle();
+console.log(firstToggle.on);
+console.groupEnd('firstToggle');
+
+const secondToggle = new Toggle();
+
+console.group('secondToggle');
+console.log(secondToggle.on);
+secondToggle.toggle();
+console.log(secondToggle.on);
+console.groupEnd('secondToggle');
 
 
